88bb3110881f5dd8ff8f22be79651ba6299eadf4
ItayElf <thh123321@gmail.com>
2021-12-29 14:59:59 +0200

    fixed candles

--- lib/screens/phone/main_screen_phone.dart
+++ lib/screens/phone/main_screen_phone.dart
@@ -6,67 +6,69 @@ class MainScreenPhone extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final int candles = MediaQuery.of(context).size.width ~/ 30;
+    const candleWidth = 30;
+    final int candles = MediaQuery.of(context).size.width ~/ candleWidth;
     return Material(
-      child: Stack(
+      child: Column(
         children: [
-          Align(
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  "assets/yellow_badge.png",
-                  width: convert(100),
-                ),
-                Text(
-                  "יום השואה הבינלאומי\n איש שלום",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headline3,
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "הדלקת נר עם שם",
-                    style: Theme.of(context).textTheme.headline4,
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    "assets/yellow_badge.png",
+                    width: convert(100),
+                  ),
+                  Text(
+                    "יום השואה הבינלאומי\n איש שלום",
                     textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline3,
                   ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
+                  const SizedBox(
+                    height: 25,
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "הדלקת נר כללי",
-                    style: Theme.of(context).textTheme.headline4,
-                    textAlign: TextAlign.center,
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "הדלקת נר עם שם",
+                      style: Theme.of(context).textTheme.headline4,
+                      textAlign: TextAlign.center,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                    ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
+                  const SizedBox(
+                    height: 25,
                   ),
-                ),
-              ],
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "הדלקת נר כללי",
+                      style: Theme.of(context).textTheme.headline4,
+                      textAlign: TextAlign.center,
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-          Positioned(
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: List.generate(
                 candles,
                 (i) => Image.asset(
-                  "candle.gif",
-                  height: MediaQuery.of(context).size.height / 6.4,
+                  "assets/candle.gif",
+                  width: candleWidth.toDouble(),
                 ),
               ),
             ),
-            bottom: 0,
           ),
         ],
       ),
