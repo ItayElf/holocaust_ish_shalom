fe30692631bde5487044ae16da018ed26e4bb1c3
ItayElf <thh123321@gmail.com>
2021-12-31 08:46:50 +0200

    person orm

---
+++ lib/classes/person.dart
@@ -0,0 +1,15 @@
+class Person {
+  String name;
+  int times;
+
+  Person(this.name) : times = 0;
+
+  Person.fromJson(Map<String, dynamic> json)
+      : name = json["name"],
+        times = json["times"];
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "times": times,
+      };
+}
---
+++ lib/services/person_orm.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:holocaust_ish_shalom/classes/person.dart';
+
+class PersonOrm {
+  static final _people = FirebaseFirestore.instance.collection("people");
+
+  static Stream<List<Person>> get people =>
+      _people.snapshots().map(_personFromSnap);
+
+  static List<Person> _personFromSnap(QuerySnapshot snapshot) {
+    return snapshot.docs.map<Person>((e) {
+      final Map<String, dynamic> json = e.data() as Map<String, dynamic>;
+      return Person.fromJson(json);
+    }).toList();
+  }
+
+  static Future<Person> getPerson(String name) async {
+    final data = (await _people.doc(name).get()).data();
+    if (data == null) {
+      throw Exception("Person $name was not found");
+    }
+    return Person.fromJson(data);
+  }
+
+  static Future<void> updateOrAddPerson(Person person) async {
+    _people.doc(person.name).set(person.toJson());
+  }
+}
